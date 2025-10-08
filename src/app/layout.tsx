3246e9ecd3f45ee5067fbb43e1de9d02f312d183
nexora-w <kitchi734@gmail.com>
2025-10-08 19:58:20 +0900

    hot fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "CoupleAgent",
-  description: "CoupleAgent",
+  description: "",
 };
 
 export default function RootLayout({
